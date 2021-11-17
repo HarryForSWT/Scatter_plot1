a2f3b028c289e39ab8755f9d058e56ef120c8d8b
Glitch (hello-webpage) <none>
2021-11-17 21:31:00 +0000

    🌚🏘 Checkpoint ./main.js:38731693/3374

--- main.js
+++ main.js
@@ -2,54 +2,53 @@ var d3; // Minor workaround to avoid error messages in editors
 
 // Waiting until document has loaded
 window.onload = () => {
-
   // YOUR CODE GOES HERE
   console.log("YOUR CODE GOES HERE");
-  const svg =d3.select('svg');
-  //svg.style('background-color','red');
-  const width = +svg.attr('width');
-  const height = +svg.attr('height');
-  
+  const svg = d3.select("svg");
+  svg.style('background-color','#311999');
+  const width = +svg.attr("width");
+  const height = +svg.attr("height");
+
   const render = data => {
-    
-    const xScale = d3.scaleLinear()
-      .domain([0, d3.max(data, d =>d.Dealer_Cost)])
-      .range([0,width]);
-    const yScale = d3.scaleBand()
-      .domain(data.map(d=>d.Name))
-      .range([0,height]);
+    const xValue = d => d.Dealer_Cost;
+    const yValue = d => d.Name;
     
     
-    //console.log(yScale.domain());
-    
-    
-    svg.selectAll('rect').data(data)
-    .enter().append('rect')
-            .attr('width',d => xScale(d.Dealer_Cost))
-            .attr('height',yScale.b
-     
+    const xScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, xValue)])
+      .range([0, width]);
+    const yScale = d3
+      .scaleBand()
+      .domain(data.map(yValue))
+      .range([0, height]);
+
+    svg.selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("y", d => yScale(yValue(d)))
+      .attr("width", d => xScale(xValue(d)))
+      .attr("height", yScale.bandwidth());
   };
-  
-  
-  
-  d3.csv('cars.csv').then(data =>{
-    data.forEach(d =>{
-      d.Retail_Price= +d.Retail_Price;
+
+  d3.csv("cars.csv").then(data => {
+    data.forEach(d => {
+      d.Retail_Price = +d.Retail_Price;
       d.Dealer_Cost = +d.Dealer_Cost;
       d.Engine_Size = +d.Engine_Size;
-      d.City_Miles_Per_Gallon =+d.City_Miles_Per_Gallon;
-      d.Cyl= +d.Cyl;
+      d.City_Miles_Per_Gallon = +d.City_Miles_Per_Gallon;
+      d.Cyl = +d.Cyl;
       d.Horsepower = +d.Horsepower;
-      d.Horsepower= +d.Horsepower;
+      d.Horsepower = +d.Horsepower;
       d.Highway_Miles_Per_Gallon = +d.Highway_Miles_Per_Gallon;
-      d.Weight= +d.Weight;
-      d.Wheel_Base= +d.Wheel_Base;
+      d.Weight = +d.Weight;
+      d.Wheel_Base = +d.Wheel_Base;
       d.Len = +d.Len;
       d.Width = +d.Width;
-    })
+    });
     render(data);
-  }) 
-  
-  // Load the data set from the assets folder:
+  });
 
+  // Load the data set from the assets folder:
 };
