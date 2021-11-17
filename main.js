9de0794dba81368d42f443d500c6f3842a207fb1
Glitch (hello-webpage) <none>
2021-11-17 20:05:49 +0000

    👐🐩 Checkpoint ./main.js:38731693/846 ./d3.v5.min.js:38731693/1370756 ./cars.csv:38731693/8

--- main.js
+++ main.js
@@ -6,7 +6,28 @@ window.onload = () => {
   // YOUR CODE GOES HERE
   console.log("YOUR CODE GOES HERE");
  
+  const render = data => {
+    
+     
+  };
+  
+  
+  
   d3.csv('cars.csv').then(data =>{
+    data.forEach(d =>{
+      d.Retail_Price= +d.Retail_Price;
+      d.Dealer_Cost = +d.Dealer_Cost;
+      d.Engine_Size = +d.Engine_Size;
+      d.City_Miles_Per_Gallon =+d.City_Miles_Per_Gallon;
+      d.Cyl= +d.Cyl;
+      d.Horsepower = +d.Horsepower;
+      d.Horsepower= +d.Horsepower;
+      d.Highway_Miles_Per_Gallon = +d.Highway_Miles_Per_Gallon;
+      d.Weight= +d.Weight;
+      d.Wheel_Base= +d.Wheel_Base;
+      d.Len = +d.Len;
+      d.Width = +d.Width;
+    })
     console.log(data);
   }) 
   
