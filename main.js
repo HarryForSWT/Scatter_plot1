933186eb059b34ddd66d49e8f0c2a619eee92d9f
Glitch (hello-webpage) <none>
2021-11-17 19:53:47 +0000

    🌒🖼 Checkpoint ./main.js:38731693/81 ./cars.csv:38731693/24

--- main.js
+++ main.js
@@ -5,16 +5,10 @@ window.onload = () => {
 
   // YOUR CODE GOES HERE
   console.log("YOUR CODE GOES HERE");
-
-  let fuk = function(){
-    var a=1;
-    var b=2;
-    console.log(a+b);
-    
-  }
+ 
   d3.csv('cars.csv').then(data =>{
     console.log(data);
-  })
+  }) 
   
   // Load the data set from the assets folder:
 
