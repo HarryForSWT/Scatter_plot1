7a5f802dcb107316f854cd40e5704dcfa7ee0944
Glitch (hello-webpage) <none>
2021-11-18 18:44:26 +0000

    ☕️🌦 Checkpoint ./main.js:38731693/348 ./style.css:38731693/20

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ window.onload = () => {
   const height = +svg.attr("height");
   const render = data => {
     const xValue = d => d.Dealer_Cost;
-    const yValue = d => d.Name;
+    const yValue = d => d.Retail_Price
     const margin = { top: 5, right: 20, bottom: 20, left: 200 };
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
@@ -19,12 +19,12 @@ window.onload = () => {
     const xScale = d3
       .scaleLinear()
       .domain(d3.extent(data, xValue))
-      .range([0, width*2/3])
+      .range([0, innerWidth])
       .nice();
     //x "Definitionsbereich" Dateneinstellen
     const yScale = d3
       .scaleLinear()
-      .domain(data.map(yValue))
+      .domain(d3.extent(data, yValue))
       .range([0, height*2/3]);
     
     //Objekt "g" mit der generellen Translationseinstelleung
@@ -41,7 +41,7 @@ window.onload = () => {
     //y-Achse Zeichnen
     g.append("g")
       .call(yAxis)
-      .attr("transform", `translate(${innerWidth/2},30)`);
+      .attr("transform", `translate(${innerWidth/2},50)`);
     //.selectAll(' .domain').remove()
     //yAchse verschwinden mit oberer Zeile
     
@@ -62,11 +62,10 @@ window.onload = () => {
       .data(data)
       .enter()
       .append("circle")
-      .attr("cy", d => yScale(yValue(d))+yScale.bandwidth()/2)
+      .attr("cy", d => yScale(yValue(d)))
       .attr("cx", d => xScale(xValue(d)))
-      .attr("r", yScale.bandwidth()/2)
-      //.attr("fill", "transparent")
-      .attr("transform", `translate(${innerWidth/2},30)`);
+      .attr("r",7)
+      .attr("transform", `translate(${innerWidth/2},0)`);
   };
 
   
